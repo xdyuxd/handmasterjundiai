ac2afd464731dccaeb51d07cdc37e5047a7b41b2
Yuri Cappellano Kaminski <yyurickk@hotmail.com>
2020-02-01 15:26:16 -0300

    Editing navbar and adding pages

---
+++ Pages/Sponsors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace handmasterjundiai.Pages
+{
+    public class SponsorsModel : PageModel
+    {
+        private readonly ILogger<SponsorsModel> _logger;
+
+        public SponsorsModel(ILogger<SponsorsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Pages/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace handmasterjundiai.Pages
+{
+    public class TeamModel : PageModel
+    {
+        private readonly ILogger<TeamModel> _logger;
+
+        public TeamModel(ILogger<TeamModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

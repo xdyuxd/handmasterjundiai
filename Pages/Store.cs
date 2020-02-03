d4ad02405f8bacb38d7e1e0cf66de4ccf29ef69e
xdyuxd <yyurickk@hotmail.com>
2020-02-03 16:12:37 -0300

    Editing index layout and adding other menus

---
+++ Pages/Store.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace handmasterjundiai.Pages
+{
+    public class StoreModel : PageModel
+    {
+        private readonly ILogger<StoreModel> _logger;
+
+        public StoreModel(ILogger<StoreModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
